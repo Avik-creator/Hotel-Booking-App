a9701cfd25be227e04fb2fc4afb784fa182792f0
Avik-creator <avikm744@gmail.com>
2024-03-08 20:56:27 +0530

    Added Hotel Routes to Add And Get the Hotels.

---
+++ backend/src/controller/hotel.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import Hotel from "../models/hotels.models";
+
+export const getAllHotels = async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ userId: req.userId });
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+};
+
+export const getInvididualHotel = async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.findOne({
+      _id: id,
+      userId: req.userId,
+    });
+    res.json(hotel);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import userRoutes from "./routes/auth.routes";
+import hotelRoutes from "./routes/myHotels.routes";
 import cookieParser from "cookie-parser";
 import { connectToDB } from "./utils/connectToDB";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-console.log(process.env.FRONTEND_URL);
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "16kb" }));
 
 app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/my-hotels", hotelRoutes);
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
